74a4464f5ac4452ed5f8c9de7d645ce9cf8a16fd
Matt Hyclak <matt.hyclak@cbts.net>
2015-08-25 12:38:32 -0400

    Fix params class

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,8 @@
 # == Class: mock::params
 #
 class jenkins_rpm_builder::params {
-  $installdir = '/usr/libexec/jenkins-rpm-builder',
-  $repo_url_prefix = 'http://yum/repo',
-  $rpm_passphrase = '',
+  $installdir = '/usr/libexec/jenkins-rpm-builder'
+  $repo_url_prefix = 'http://yum/repo'
+  $rpm_passphrase = ''
 }
 
